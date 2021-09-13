07bcce1f7f71f63ebbbb8ec10b1a41126f4c0948
Maryam-ask <maryamaskari19@gmail.com>
2021-09-13 12:53:26 +0200

    eight_puzzle file is added with a test file for testing the puzzle

--- eight_puzzle.py
+++ eight_puzzle.py
@@ -7,11 +7,11 @@ from copy import deepcopy
 
 
 class EightPuzzle:
-    def __init__(self, initial_state, goal):
+    def _init_(self, initial_state, goal, eight_value="e"):
         self.state = initial_state
+        self.eight_value = eight_value
         self.goal = goal
         self.action = ["up", "down", "left", "right"]
-        self.e_position = self.state[1][1]
 
     def check_goal(self):
         if self.state == self.goal:
@@ -19,16 +19,65 @@ class EightPuzzle:
         else:
             return False
 
+    def get_e_position(self, index):
+        for i in range(len(self.state)):
+            for j in range(len(self.state[i])):
+                if self.state[i][j] == self.eight_value:
+                    return (i, j)[index]
+
     def move(self, move):
+        dc = deepcopy(self)
         if move == "up":
-            dc = deepcopy(self)
+            if dc.up():
+                return dc
+        elif move == "down":
+            if dc.down():
+                return dc
+        elif move == "right":
+            if dc.right():
+                return dc
+        elif move == "left":
+            if dc.left():
+                return dc
+
+    def up(self):
+        i = self.get_e_position(0)
+        j = self.get_e_position(1)
+        if 0 < i < len(self.state):
+            self.state[i][j] = self.state[i - 1][j]
+            self.state[i - 1][j] = self.eight_value
+            return True
+
+    def down(self):
+        i = self.get_e_position(0)
+        j = self.get_e_position(1)
+        if 0 <= i < len(self.state):
+            self.state[i][j] = self.state[i + 1][j]
+            self.state[i + 1][j] = self.eight_value
+            return True
+
+    def right(self):
+        i = self.get_e_position(0)
+        j = self.get_e_position(1)
+        if 0 < j < len(self.state[i]):
+            self.state[i][j] = self.state[i][j + 1]
+            self.state[i][j + 1] = self.eight_value
+            return True
+
+    def left(self):
+        i = self.get_e_position(0)
+        j = self.get_e_position(1)
+        if 0 <= j < len(self.state[i]):
+            self.state[i][j] = self.state[i][j + 1]
+            self.state[i][j + 1] = self.eight_value
+            return True
 
     def pretty_print(self):
         """
         A Method to print out the states
         """
-        print('----------------------------')
-        print("[", self.state[0][0], ",", self.state[0][1], ",", self.state[0][2], "]", ","
-              "[", self.state[1][0], ",", self.state[1][1], ",", self.state[1][2], "]", ",",
-              "[", self.state[2][0], ",", self.state[2][1], ",", self.state[2][2], "]]")
-        print('----------------------------')
+        print("-------------------------------")
+        for j in self.state:
+            [print(i, end="\t\t") for i in j]
+            print()
+        print("-------------------------------")
--- test.py
+++ test.py
@@ -13,6 +13,9 @@ from eight_puzzle import EightPuzzle
 initial_state = [[7, 2, 4], [5, 'e', 6], [8, 3, 1]]
 goal_state = [['e', 1, 2], [3, 4, 5], [6, 7, 8]]
 
-puzzle = EightPuzzle(initial_state,goal_state)
+#puzzle = EightPuzzle(initial_state,goal_state)
 print("Start State: ")
-puzzle.pretty_print()
+# puzzle.pretty_print()
+
+
+print(e_position)
