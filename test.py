898c2c9f831dacb1de26dfb6203d98690115a3b9
Maryam-ask <maryamaskari19@gmail.com>
2021-09-16 01:31:47 +0200

    test the manhattan distance method h_2()

--- test.py
+++ test.py
@@ -9,13 +9,14 @@
 # =============================================================================
 
 from eight_puzzle import EightPuzzle
+from node_and_search import Node
 
 initial_state = [[7, 2, 4], [5, 'e', 6], [8, 3, 1]]
 goal_state = [['e', 1, 2], [3, 4, 5], [6, 7, 8]]
 
-#puzzle = EightPuzzle(initial_state,goal_state)
+puzzle = EightPuzzle(initial_state,goal_state)
 print("Start State: ")
-puzzle.pretty_print()
-
-
-print(e_position)
+#puzzle.pretty_print()
+node = Node(puzzle)
+print(node.h_2())
+#print(e_position)
