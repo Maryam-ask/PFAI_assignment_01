9e4eb9ccc6e92ea5b51e0673caf6085e1b1842cc
Maryam-ask <maryamaskari19@gmail.com>
2021-09-18 22:58:47 +0200

    comment

--- run_assignmnet_1.py
+++ run_assignmnet_1.py
@@ -13,21 +13,39 @@ from eight_puzzle import EightPuzzle
 
 
 def main():
+    """
+    The first codes are used to solve the missionaries and cannibals problem
+    and the other part is to solve the eight_puzzle problem.
+    """
     init_state = [[0, 0], 'r', [3, 3]]
     goal_state = [[3, 3], 'l', [0, 0]]
     mc = MissionariesAndCannibals(init_state, goal_state)
     sa = SearchAlgorithm(mc)
-    # print(sa)
-    # print(mc)
-    # solution = sa.bfs(True)
-    # solution.pretty_print_solution1(False)
-    #solution.pretty_print_solution(True)
+    """
+    To print the solution through IDS search:
+    """
+    #mc.pretty_print()
+    #solution = sa.bfs()
+    #solution = sa.bfs(True)  # BFS using Statistics = True
+    #solution.pretty_print_solution1(False)    # verbose = False for only printing actions
+    #solution.pretty_print_solution(True)   # Verbose = True for printing actions and states
 
-    # solution = sa.dfs(sa.start, [], False, True)
-    # solution.pretty_print_solution(True)
+    """
+    To print the solution through DFS search
+    """
+    # mc.pretty_print()
+    #solution = sa.dfs(sa.start, [], False)
+    #solution = sa.dfs(sa.start, [], False, True)   # Statistics = True
+    #solution.pretty_print_solution(True)   # Verbose = True
+    #solution.pretty_print_solution()       # verbose = False
 
+    """
+    To print the solution through IDS search 
+    """
     # solution = sa.ids(True)
     # solution.pretty_print_solution(True)
+
+    # Eight_puzzle
     initial_state = [[7, 2, 4], [5, 'e', 6], [8, 3, 1]]
     goal_state = [['e', 1, 2], [3, 4, 5], [6, 7, 8]]
 
@@ -40,8 +58,8 @@ def main():
     sa = SearchAlgorithm(puzzle)
     # solution = sa.ids(True)
     # solution.pretty_print_solution(True)
-    solution = sa.greedy_search()
-    solution.pretty_print_solution(True)
+    # solution = sa.greedy_search()
+    # solution.pretty_print_solution(True)
     #print(dfs_rec.action, "**: ", dfs_rec.state.state)
     #print(dfs_rec)
     # if solution:
