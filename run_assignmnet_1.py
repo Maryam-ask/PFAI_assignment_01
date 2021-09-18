10b9d5a93240eae3a718be0491634676a2df51ab
mahtabbm <mahtab.bmohammadi@gmail.com>
2021-09-19 00:17:53 +0430

    Greedy Search is Done + pretty prints works for every algorithms in node_and_search.py

--- run_assignmnet_1.py
+++ run_assignmnet_1.py
@@ -9,12 +9,12 @@ Completed by: Maryam Askari & Mahtab BMohammadi
 
 from missionaries_and_cannibals import MissionariesAndCannibals
 from node_and_search import SearchAlgorithm
-
-init_state = [[0, 0], 'r', [3, 3]]
-goal_state = [[3, 3], 'l', [0, 0]]
+from eight_puzzle import EightPuzzle
 
 
 def main():
+    init_state = [[0, 0], 'r', [3, 3]]
+    goal_state = [[3, 3], 'l', [0, 0]]
     mc = MissionariesAndCannibals(init_state, goal_state)
     sa = SearchAlgorithm(mc)
     # print(sa)
@@ -26,7 +26,21 @@ def main():
     # solution = sa.dfs(sa.start, [], False, True)
     # solution.pretty_print_solution(True)
 
-    solution = sa.ids(True)
+    # solution = sa.ids(True)
+    # solution.pretty_print_solution(True)
+    initial_state = [[7, 2, 4], [5, 'e', 6], [8, 3, 1]]
+    goal_state = [['e', 1, 2], [3, 4, 5], [6, 7, 8]]
+
+    puzzle = EightPuzzle(initial_state, goal_state)
+    # print("Start State: ")
+    # puzzle.pretty_print()
+    # print(puzzle.h_2())
+    # print(puzzle.h_1())
+    # print(e_position)
+    sa = SearchAlgorithm(puzzle)
+    # solution = sa.ids(True)
+    # solution.pretty_print_solution(True)
+    solution = sa.greedy_search()
     solution.pretty_print_solution(True)
     #print(dfs_rec.action, "**: ", dfs_rec.state.state)
     #print(dfs_rec)
